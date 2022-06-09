3fa27a4b054b677f6b9921bf00326b7ce305a910
Ethan Rahimian <nicootech@gmail.com>
2022-06-09 11:56:21 -0700

    2- dependencies and view binding

--- app/src/main/java/com/nicootech/simplehiltpractice/MainActivity.kt
+++ app/src/main/java/com/nicootech/simplehiltpractice/MainActivity.kt
@@ -2,10 +2,19 @@ package com.nicootech.simplehiltpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.nicootech.simplehiltpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val TAG: String = "AppDebug"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
